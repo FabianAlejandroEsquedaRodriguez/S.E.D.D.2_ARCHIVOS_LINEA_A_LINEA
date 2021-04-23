7ffa021743a37998153b50484231b97f1a2a1b72
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2021-04-22 20:37:27 -0500

    Se implemento un pequeño algoritmo para leer la informacion de un archivo de texto

---
+++ main.py
@@ -0,0 +1,12 @@
+#Leer informacion de un archivo Linea por Linea
+
+#Paso 1 -> Abrir el archivo
+Archivo = open("Letra_de_mi_cancion_favorita.txt", 'r')#Abre el archivo en modo de lectura
+#Crea una lista, linea a linea con lo que tiene el archivo
+
+#Paso 2 -> Procesar el archivo
+for linea in Archivo:
+    print(linea)
+
+#Paso 3 -> Cerrar el archivo
+Archivo.close()
